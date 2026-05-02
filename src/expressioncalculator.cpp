b1eebe4cd0586dd215482e8fde3da2f674a63910
xaatim <xayari229@gmail.com>
2026-05-02 08:55:15 +0300

    added shunting yard algorithm

--- src/expressioncalculator.cpp
+++ src/expressioncalculator.cpp
@@ -1,36 +1,83 @@
 #include <cstdlib>
 #include <iostream>
+#include <queue>
 #include <stack>
 #include <tuple>
+#include <cmath>
 
-std::tuple<char, int> expressionChecker(std::string userInput) {
-  if (userInput.find('+',1) < userInput.length()) {
-    return std::make_tuple('+', userInput.find('+',1));
+int checkPrecedence(char op) {
+  switch (op) {
+    case '-':
+      return 1;
+      break;
+
+    case '+':
+      return 1;
+      break;
+
+    case '*':
+      return 2;
+      break;
+
+    case '/':
+      return 2;
+      break;
+
+      case '^':
+      return 3;
+      break;
+      
+    default:
+      break;
+
   }
-  if (userInput.find('-',1) < userInput.length()) {
-    return std::make_tuple('-', userInput.find('-',1));
+  return 0;
+}
+
+void expressionChecker(std::string userInput,
+                       std::queue<std::string>& outputQueue,
+                       std::stack<char>& operatorStack) {
+  std::string tempNumBuff;
+
+  char finalChar;
+  char tempCharBuff;
+  for (int i = 0; i < userInput.length(); i++) {
+    if (isdigit(userInput[i]) || userInput[i] == '.') {
+      tempNumBuff += userInput[i];
+    }
+    if (!isdigit(userInput[i]) && userInput[i] != '.') {
+      if (!tempNumBuff.empty()) {
+        outputQueue.push(tempNumBuff);
+        tempNumBuff.clear();
+      }
+
+      while (!operatorStack.empty() && checkPrecedence(operatorStack.top()) >=
+                                           checkPrecedence(userInput[i])) {
+        tempCharBuff = operatorStack.top();
+        operatorStack.pop();
+        outputQueue.push(std::string(1, tempCharBuff));
+      }
+      operatorStack.push(userInput[i]);
+    }
   }
-  if (userInput.find('/',1) < userInput.length()) {
-    return std::make_tuple('/', userInput.find('/',1));
+  if (!tempNumBuff.empty()) {
+    outputQueue.push(tempNumBuff);
+    tempNumBuff.clear();
   }
-  if (userInput.find('*',1) < userInput.length()) {
-    return std::make_tuple('*', userInput.find('*',1));
+
+  while (!operatorStack.empty()) {
+    outputQueue.push(std::string(1, operatorStack.top()));
+    operatorStack.pop();
   }
-  return std::make_tuple(' ', -1);
 }
 
 bool validateInput(std::string userInput) {
   for (int i = 0; i < userInput.length(); i++) {
     if (userInput[i] == '+' || userInput[i] == '-' || userInput[i] == '/' ||
-        userInput[i] == '*' || userInput[i]=='.') {
+        userInput[i] == '*' || userInput[i] == '.'|| userInput[i] == '^') {
       continue;
     }
 
-    // if (atoi(userInput[i]) == 0) {
-    // std::cout << "unkown varialbe: " << "\"" << userInput[i]<< "\"" <<
-    // std::endl; return true;
-
-    // }
     if (!std::isdigit(userInput[i])) {
       std::cout << "unkown varialbe: " << "\"" << userInput[i] << "\""
                 << std::endl;
@@ -41,82 +88,80 @@ bool validateInput(std::string userInput) {
 }
 
 int main() {
-  std::stack<double> numSt;
+  std::stack<char> operatorStack;
+  std::stack<double> solveStack;
+  std::queue<std::string> outputQueue;
+
   double firstOperand = 0;
   double secondOperand = 0;
   double result;
-
-  int expressionDigit;
-  char expressionType;
-
-  std::string charsBeforeOperator;
-  std::string charsAfterOperator;
+  double tempIntHolder;
   std::string userInput;
 
   std::cout << "expression calculator: ";
   std::cin >> userInput;
 
   if (!validateInput(userInput)) {
-    std::tie(expressionType, expressionDigit) = expressionChecker(userInput);
-    std::cout << "expressionDigit: " << expressionDigit << std::endl;
-    std::cout << "expressionType : " << expressionType << std::endl;
+    expressionChecker(userInput, outputQueue, operatorStack);
+    while (!outputQueue.empty()) {
+      std::string token = outputQueue.front();
+      outputQueue.pop();
+      if (isdigit(token[0])) {
+        solveStack.push(stod(token));
+      } else {
+        switch (token[0]) {
+          case '+':
+            secondOperand = solveStack.top();
+            solveStack.pop();
+            firstOperand = solveStack.top();
+            solveStack.pop();
+            result = firstOperand + secondOperand;
+            solveStack.push(result);
+            break;
 
-    if (expressionDigit == -1) {
-      result = stod(userInput);
-      std::cout << "restult: " << result << std::endl;
-      return 0;
+          case '-':
+            secondOperand = solveStack.top();
+            solveStack.pop();
+            firstOperand = solveStack.top();
+            solveStack.pop();
+            result = firstOperand - secondOperand;
+            solveStack.push(result);
+            break;
+
+          case '/':
+            secondOperand = solveStack.top();
+            solveStack.pop();
+            firstOperand = solveStack.top();
+            solveStack.pop();
+            result = firstOperand / secondOperand;
+            solveStack.push(result);
+            break;
+
+          case '*':
+            secondOperand = solveStack.top();
+            solveStack.pop();
+            firstOperand = solveStack.top();
+            solveStack.pop();
+            result = firstOperand * secondOperand;
+            solveStack.push(result);
+            break;
+          case '^':
+            secondOperand = solveStack.top();
+            solveStack.pop();
+            firstOperand = solveStack.top();
+            solveStack.pop();
+            result = pow(firstOperand, secondOperand);
+            solveStack.push(result);
+            break;
+
+          default:
+            std::cout << "unkown variable:" << std::endl;
+            break;
+
+        }
+      }
     }
-    for (int i = 0; i < expressionDigit; i++) {
-      charsBeforeOperator += userInput[i];
-    }
-    numSt.push(std::stod(charsBeforeOperator));
-
-    std::cout << "charsBeforeOperator: " << charsBeforeOperator << std::endl;
-
-    for (int i = expressionDigit + 1; i < userInput.length(); i++) {
-      charsAfterOperator += userInput[i];
-    }
-
-    numSt.push(std::stod(charsAfterOperator));
-    std::cout << "charsAfterOperator: " << charsAfterOperator << std::endl;
-
-    switch (expressionType) {
-      case '+':
-        secondOperand = numSt.top();
-        numSt.pop();
-        firstOperand = numSt.top();
-        result = firstOperand + secondOperand;
-        std::cout << "restult: " << result << std::endl;
-        break;
-
-      case '-':
-        secondOperand = numSt.top();
-        numSt.pop();
-        firstOperand = numSt.top();
-        result = firstOperand - secondOperand;
-        std::cout << "restult: " << result << std::endl;
-        break;
-
-      case '/':
-        secondOperand = numSt.top();
-        numSt.pop();
-        firstOperand = numSt.top();
-        result = firstOperand / secondOperand;
-        std::cout << "restult: " << result << std::endl;
-        break;
-
-      case '*':
-        secondOperand = numSt.top();
-        numSt.pop();
-        firstOperand = numSt.top();
-        result = firstOperand * secondOperand;
-        std::cout << "restult: " << result << std::endl;
-        break;
-
-      default:
-        std::cout << "unkown variable:" << std::endl;
-        break;
-    }
-    return 0;
   }
+  std::cout << "result: " << solveStack.top()<< std::endl;
+  return 0;
 }
